54a1b8b9c2248bf64d097011057663e9bff6249f
VictorOdukoya <victor.odukoya@outlook.de>
2024-03-10 03:39:27 +0100

    added theme switcher component and restyled pages based on new design

--- victor-odukoya/src/app/app.module.ts
+++ victor-odukoya/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { ScullyLibModule } from '@scullyio/ng-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ScullyLibModule,
-  ],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, ScullyLibModule, ThemeSwitcherComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
