3dd818e054db5beb7461180f2a90fb45b45ab501
VictorOdukoya <victor.odukoya@outlook.de>
2024-03-12 21:37:14 +0100

    added techstack component and adjusted some stylings

--- victor-odukoya/src/app/components/+pages/home/home.module.ts
+++ victor-odukoya/src/app/components/+pages/home/home.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SkillCardContainerComponent } from '../../skill-card-container/skill-card-container.component';
 import { SkillCardComponent } from '../../skill-card-container/skill-card/skill-card.component';
+import { TechStackBannerComponent } from '../../tech-stack-banner/tech-stack-banner.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, FontAwesomeModule, SkillCardContainerComponent, SkillCardComponent]
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    FontAwesomeModule,
+    SkillCardContainerComponent,
+    SkillCardComponent,
+    TechStackBannerComponent
+  ]
 })
 export class HomeModule {}
---
+++ victor-odukoya/src/app/components/tech-stack-banner/tech-stack-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-stack-banner',
+  standalone: true,
+  templateUrl: './tech-stack-banner.component.html',
+  styleUrls: ['./tech-stack-banner.component.scss']
+})
+export class TechStackBannerComponent {
+}
