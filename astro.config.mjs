08c62703b7b1e18f5353d3adfba3b5cd961d34e9
VictorOdukoya <victor.odukoya@outlook.de>
2026-01-17 03:11:48 +0100

    Update support and terms pages with new URLs and styles; remove scroll indicator from Hero component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://victorodukoya.github.io',
-  base: '/',
+  site: "https://victorodukoya.github.io",
+  base: "/",
   integrations: [tailwind()],
-  output: 'static',
-  outDir: './docs',
+  output: "static",
+  outDir: "./docs",
   build: {
-    assets: 'assets',
+    assets: "assets",
   },
 });
