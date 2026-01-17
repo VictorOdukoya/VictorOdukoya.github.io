f08e66688e728ffcfd45623e53aa499eabfb661c
VictorOdukoya <victor.odukoya@outlook.de>
2026-01-17 03:03:00 +0100

    Replace Angular/Scully website with new Astro Pairloom website
    - Removed old Angular project (victor-odukoya folder)
    - Replaced docs folder content with new Astro build output
    - Cleaned up root config files (prettierrc, yarn.lock, etc.)
    - Removed node_modules

--- victor-odukoya/src/app/interfaces/custom-scully-route.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ScullyRoute } from "@scullyio/ng-lib";
-
-export interface CustomScullyRoute extends ScullyRoute{
-    description: string;
-    author: string;
-    tags: Array<string>;
-    image: string;
-    createdAt: string;
-}
